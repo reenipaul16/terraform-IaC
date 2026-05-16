6de64e620218686bf6e1b7414f75e09961ee0013
Reeni Paul <paulreeni1616@gmail.com>
2026-05-16 21:25:14 +0530

    commit3

--- terraform/2.VPC/main.tf
+++ terraform/2.VPC/main.tf
@@ -1,6 +1,9 @@
 module "network" {
-  source = "../Modules/network"
+  source = "../Modules/VPC"
 
   project_id = var.project-id
   region     = var.region
+  network_name= var.network_name
+  subnet_name = var.subnet_name
+
 }
