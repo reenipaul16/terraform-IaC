b9b2f5e535850e5cae3dbf7a75a8f3b4f79c5a79
Reeni Paul <paulreeni1616@gmail.com>
2026-05-24 19:44:16 +0530

    ar1

--- terraform/Modules/ArtifactRegistry/main.tf
+++ terraform/Modules/ArtifactRegistry/main.tf
@@ -2,4 +2,10 @@ resource "google_artifact_registry_repository" "app_repository" {
   location      = var.location
   repository_id = var.repository_id
   format        = "DOCKER"
+
+  cleanup_policy_dry_run = true
+
+  docker_config {
+    immutable_tags = false
+  }
 }
